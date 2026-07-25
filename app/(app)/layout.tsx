648a3ec81eccdd78c085c309f9f224d5ee5660c7
vidainternacional <publiartsv.info@gmail.com>
2026-07-25 12:17:48 -0600

    fix(biblia): integrar selector claro de versión y capítulo

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import BottomNav from '@/components/layout/BottomNav'
+import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
 
 export default function AppLayout({
   children,
@@ -7,6 +8,7 @@ export default function AppLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <BibleSelectorPolish />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
         {children}
