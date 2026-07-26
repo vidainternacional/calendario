7fd4dbe008cbd8fe135643060586d24547d765fe
vidainternacional <publiartsv.info@gmail.com>
2026-07-26 11:19:20 -0600

    feat(mobile): activar pulido global de tablas

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,6 +5,7 @@ import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
 import BibleVerseActionsNoFlash from '@/components/biblia/BibleVerseActionsNoFlash'
 import BibleVerseActionsPersistent from '@/components/biblia/BibleVerseActionsPersistent'
 import BottomNav from '@/components/layout/BottomNav'
+import MobileTablePolish from '@/components/layout/MobileTablePolish'
 
 export default function AppLayout({
   children,
@@ -12,14 +13,15 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <BibleVerseActionsNoFlash />
       <BibleVerseActionsPersistent />
       <BibleNotesPrefetch />
       <BibleSelectorPolish />
       <BibleExperienceFixes />
       <BibleCompareAndActionsPolish />
-      <div className="flex-1 pb-16">
+      <MobileTablePolish />
+      <div data-mobile-table-scope="true" className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
         {children}
       </div>
