a9908eda7880ed9a0ff4efa4ff451ae3bc4bb2b6
vidainternacional <publiartsv.info@gmail.com>
2026-07-25 23:38:08 -0600

    fix(biblia): activar comparación visible y acciones minimalistas

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import BibleCompareAndActionsPolish from '@/components/biblia/BibleCompareAndActionsPolish'
 import BibleExperienceFixes from '@/components/biblia/BibleExperienceFixes'
 import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
 import BottomNav from '@/components/layout/BottomNav'
@@ -11,6 +12,7 @@ export default function AppLayout({
     <div className="flex flex-col min-h-screen">
       <BibleSelectorPolish />
       <BibleExperienceFixes />
+      <BibleCompareAndActionsPolish />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
         {children}
