e9450c7a35447051d447213ac6c5cb0aad3baeee
vidainternacional <publiartsv.info@gmail.com>
2026-07-26 20:01:11 -0600

    revert(biblia): restaurar layout del último estado estable

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,11 +2,9 @@ import BibleCompareAndActionsPolish from '@/components/biblia/BibleCompareAndAct
 import BibleExperienceFixes from '@/components/biblia/BibleExperienceFixes'
 import BibleNotesPrefetch from '@/components/biblia/BibleNotesPrefetch'
 import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
-import BibleVerseActionIconsReady from '@/components/biblia/BibleVerseActionIconsReady'
 import BibleVerseActionsNoFlash from '@/components/biblia/BibleVerseActionsNoFlash'
+import BibleVerseActionsPersistent from '@/components/biblia/BibleVerseActionsPersistent'
 import BottomNav from '@/components/layout/BottomNav'
-import MobileFormPolish from '@/components/layout/MobileFormPolish'
-import MobileTablePolish from '@/components/layout/MobileTablePolish'
 
 export default function AppLayout({
   children,
@@ -14,20 +12,14 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex flex-col min-h-screen">
       <BibleVerseActionsNoFlash />
-      <BibleVerseActionIconsReady />
+      <BibleVerseActionsPersistent />
       <BibleNotesPrefetch />
       <BibleSelectorPolish />
       <BibleExperienceFixes />
       <BibleCompareAndActionsPolish />
-      <MobileTablePolish />
-      <MobileFormPolish />
-      <div
-        data-mobile-table-scope="true"
-        data-mobile-form-scope="true"
-        className="flex-1 pb-16"
-      >
+      <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
         {children}
       </div>
