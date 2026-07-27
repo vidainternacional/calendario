ff6490905f1d44d4b1df9d9edef725f1b99c817d
vidainternacional <publiartsv.info@gmail.com>
2026-07-27 14:55:06 -0600

    refactor(biblia): cargar mejoras desde la página única

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,3 @@
-import BibleCompareAllVersions from '@/components/biblia/BibleCompareAllVersions'
-import BibleCompareAndActionsPolish from '@/components/biblia/BibleCompareAndActionsPolish'
-import BibleEmbeddedParity from '@/components/biblia/BibleEmbeddedParity'
-import BibleExperienceFixes from '@/components/biblia/BibleExperienceFixes'
-import BibleNotesPrefetch from '@/components/biblia/BibleNotesPrefetch'
-import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
-import BibleVerseActionsNoFlash from '@/components/biblia/BibleVerseActionsNoFlash'
-import BibleVerseActionsPersistent from '@/components/biblia/BibleVerseActionsPersistent'
 import BottomNav from '@/components/layout/BottomNav'
 import PastoralEmbeddedBiblePolish from '@/components/pastoral/PastoralEmbeddedBiblePolish'
 
@@ -15,15 +7,7 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <BibleVerseActionsNoFlash />
-      <BibleVerseActionsPersistent />
-      <BibleNotesPrefetch />
-      <BibleSelectorPolish />
-      <BibleExperienceFixes />
-      <BibleCompareAndActionsPolish />
-      <BibleCompareAllVersions />
-      <BibleEmbeddedParity />
+    <div className="flex min-h-screen flex-col">
       <PastoralEmbeddedBiblePolish />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
