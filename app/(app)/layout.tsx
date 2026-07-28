2f087f3b64c77332a02843991f5fa6747f5ed599
vidainternacional <publiartsv.info@gmail.com>
2026-07-27 21:53:18 -0600

    fix(biblia): retirar capas globales que duplicaban notas

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,3 @@
-import BibleNotesInlinePolish from '@/components/biblia/BibleNotesInlinePolish'
-import BibleUnifiedWorkspacePanels from '@/components/biblia/BibleUnifiedWorkspacePanels'
 import BottomNav from '@/components/layout/BottomNav'
 import PastoralEmbeddedBiblePolish from '@/components/pastoral/PastoralEmbeddedBiblePolish'
 
@@ -10,8 +8,6 @@ export default function AppLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <BibleUnifiedWorkspacePanels />
-      <BibleNotesInlinePolish />
       <PastoralEmbeddedBiblePolish />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
