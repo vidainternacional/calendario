e5958d0aa60428a1948050120ff1fdc710a88024
vidainternacional <publiartsv.info@gmail.com>
2026-07-27 18:28:27 -0600

    fix(biblia): activar paneles unificados en general y pastoral

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+import BibleNotesInlinePolish from '@/components/biblia/BibleNotesInlinePolish'
+import BibleUnifiedWorkspacePanels from '@/components/biblia/BibleUnifiedWorkspacePanels'
 import BottomNav from '@/components/layout/BottomNav'
 import PastoralEmbeddedBiblePolish from '@/components/pastoral/PastoralEmbeddedBiblePolish'
 
@@ -8,6 +10,8 @@ export default function AppLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
+      <BibleUnifiedWorkspacePanels />
+      <BibleNotesInlinePolish />
       <PastoralEmbeddedBiblePolish />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
