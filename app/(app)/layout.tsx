181dfbde2abc2d181a0753f7aff1505a3566fca2
vidainternacional <publiartsv.info@gmail.com>
2026-07-27 22:06:15 -0600

    revert(biblia): restaurar layout validado

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,11 @@
+import BibleCompareAllVersions from '@/components/biblia/BibleCompareAllVersions'
+import BibleCompareAndActionsPolish from '@/components/biblia/BibleCompareAndActionsPolish'
+import BibleExperienceFixes from '@/components/biblia/BibleExperienceFixes'
+import BibleNotesPrefetch from '@/components/biblia/BibleNotesPrefetch'
+import BibleSelectorPolish from '@/components/biblia/BibleSelectorPolish'
+import BibleVerseActionsNoFlash from '@/components/biblia/BibleVerseActionsNoFlash'
+import BibleVerseActionsPersistent from '@/components/biblia/BibleVerseActionsPersistent'
 import BottomNav from '@/components/layout/BottomNav'
-import PastoralEmbeddedBiblePolish from '@/components/pastoral/PastoralEmbeddedBiblePolish'
 
 export default function AppLayout({
   children,
@@ -7,8 +13,14 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <PastoralEmbeddedBiblePolish />
+    <div className="flex flex-col min-h-screen">
+      <BibleVerseActionsNoFlash />
+      <BibleVerseActionsPersistent />
+      <BibleNotesPrefetch />
+      <BibleSelectorPolish />
+      <BibleExperienceFixes />
+      <BibleCompareAndActionsPolish />
+      <BibleCompareAllVersions />
       <div className="flex-1 pb-16">
         {/* pb-16 to account for the bottom nav height */}
         {children}
