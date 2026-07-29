d420435a929122963c591fdb759850eeca0feb63
vidainternacional <publiartsv.info@gmail.com>
2026-07-29 10:34:39 -0600

    chore(pastoral): conservar una sola migración de endurecimiento

--- supabase/migrations/20260729163500_endurecer_funciones_acceso_pastoral.sql
+++
@@ -1,5 +0,0 @@
-revoke execute on function public.tiene_acceso_pastoral() from public, anon;
-grant execute on function public.tiene_acceso_pastoral() to authenticated, service_role;
-
-revoke execute on function public.proteger_asignacion_acceso_pastoral() from public, anon, authenticated;
-grant execute on function public.proteger_asignacion_acceso_pastoral() to service_role;
