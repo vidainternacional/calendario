88c357247c3b0f480b932dfa84effd2383a73e73
vidainternacional <publiartsv.info@gmail.com>
2026-07-29 10:33:08 -0600

    security(pastoral): cerrar funciones internas de autorización

---
+++ supabase/migrations/20260729163500_endurecer_funciones_acceso_pastoral.sql
@@ -0,0 +1,5 @@
+revoke execute on function public.tiene_acceso_pastoral() from public, anon;
+grant execute on function public.tiene_acceso_pastoral() to authenticated, service_role;
+
+revoke execute on function public.proteger_asignacion_acceso_pastoral() from public, anon, authenticated;
+grant execute on function public.proteger_asignacion_acceso_pastoral() to service_role;
