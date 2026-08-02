629ff3bf2149ef974ecd6df5fc8e2edc06775a97
vidainternacional <publiartsv.info@gmail.com>
2026-08-02 13:01:48 -0600

    data(fase-d): importar cinco cartas cortas del NT

---
+++ supabase/migrations/20260802104000_importar_cartas_cortas_tagnt.sql
@@ -0,0 +1,37 @@
+-- FASE D · Bloque 4
+-- Lote de cartas con correspondencia directa de versificación.
+
+select internal.import_stepbible_tagnt_book(
+  '2Jn','2JN',13,243,6,
+  '5d3fb05db9e316b86a468ecc2dba7ed21d85bb5da7c2167f78ec016bb1181172',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Jud','JUD',25,456,12,
+  'b628eae94a494c9c37305019d678750948be64e26ec90c464c6ebf1fb6232fbc',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Tit','TIT',46,658,22,
+  '6054cc4d87f58a5e3414639bba94225f6bbf3c2444a3756a3633711120fb8c60',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  '2Th','2TH',47,823,19,
+  '175ade56690eb61515a8540769a728eeec0740c4bf8c7914328e20bd3b05dadb',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  '2Pe','2PE',61,1093,19,
+  '1b559c0ad3a37e7d5d820a88df71ec3f5c8416876a07b6f346547f32ec193617',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
