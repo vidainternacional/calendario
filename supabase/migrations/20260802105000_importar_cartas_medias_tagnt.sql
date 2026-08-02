f9ed15afca81f868834321626f120de4b56323c4
vidainternacional <publiartsv.info@gmail.com>
2026-08-02 13:07:51 -0600

    data(fase-d): importar seis cartas medias del NT
    Importa Filipenses, Colosenses, 1 Tesalonicenses, Santiago, 1 Pedro y 1 Juan con 606 textos, 10,250 palabras base y 375 variantes documentadas.

---
+++ supabase/migrations/20260802105000_importar_cartas_medias_tagnt.sql
@@ -0,0 +1,44 @@
+-- FASE D · Bloque 4
+-- Lote de cartas de tamaño medio con correspondencia directa de versificación.
+
+select internal.import_stepbible_tagnt_book(
+  'Php','PHP',104,1629,30,
+  '3159ad2b499153d7b7d3d4563dff26ac6de872fd1412b1197fe56ad32a1f8851',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Col','COL',95,1582,54,
+  'dbc41704cc56b5d08ee372171b792b8f3ce2f48fd6faff9fc782b7bd4bfccca6',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  '1Th','1TH',89,1481,35,
+  'f89087b89fee2ac7532539fe9146da2a766bd558df9f6fec05f22076bb16bd05',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Jas','JAS',108,1743,36,
+  'a832b270c59740b61f0d90ad7a060562c529741a1f6df6293223c2bbc58ad977',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  '1Pe','1PE',105,1677,57,
+  '607a4eb740c4aecc2c97f929951cbe1d2b0416ca0b35366d6199038f3f550ae2',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+
+select internal.import_stepbible_tagnt_book(
+  '1Jn','1JN',105,2138,60,
+  '0b344596bb02f0d16145e34b7bbe18715e07423a32c21e35a40ac83cbad5fee3',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
