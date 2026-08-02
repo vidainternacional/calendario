e36ecb346979a22625e7661b3ef0784120b85b14
vidainternacional <publiartsv.info@gmail.com>
2026-08-02 13:08:33 -0600

    data(fase-d): importar siete cartas largas del NT

---
+++ supabase/migrations/20260802110000_importar_cartas_largas_tagnt.sql
@@ -0,0 +1,45 @@
+-- FASE D · Bloque 4
+-- Cartas restantes con correspondencia directa de referencias.
+
+select internal.import_stepbible_tagnt_book(
+  'Rom','ROM',433,7122,139,
+  '6c22ebf16c6ebc5e839188c233746df709ae3da8fcb81e4b9fee3b1abd6daa87',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  '1Co','1CO',437,6829,157,
+  '0a0f5c00f614aeef273e617d45baf759fa11a601b2eec7f65051b42bb49262ce',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  'Gal','GAL',149,2230,40,
+  '2cc2698644994019dce03ebc6a84283e6eaf578b06830e4956f2229fb9b36232',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  'Eph','EPH',155,2422,62,
+  'b6e14ad281a623f9a26be31d5c46bc69cb1c2e66e6998cb73f02f22b4258c7c8',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  '1Ti','1TI',113,1591,49,
+  'e3404ecdc61d0730cf12ddb7f66d5e07203eff14095b9c40cf7ea72a22bead13',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  '2Ti','2TI',83,1238,35,
+  '9015a9d779e10ce3006c1c7b932b2083efd903461b5fbf3bd1a20b60428f4699',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
+select internal.import_stepbible_tagnt_book(
+  'Heb','HEB',303,4953,84,
+  '9c2167955d809a060bbdddb6d6a9a90d27c0bada313044e12588db2b196119b1',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
