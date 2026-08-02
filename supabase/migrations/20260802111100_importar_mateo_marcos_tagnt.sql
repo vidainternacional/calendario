8c0c0338e399508430c7c1f751207d73a004f2eb
vidainternacional <publiartsv.info@gmail.com>
2026-08-02 13:19:34 -0600

    data(fase-d): importar Evangelios y Hechos completos
    Importa Mateo, Marcos, Lucas, Juan y Hechos con 4,786 textos, 83,308 palabras base, 4,367 variantes documentadas y trazabilidad correcta por archivo TAGNT.

---
+++ supabase/migrations/20260802111100_importar_mateo_marcos_tagnt.sql
@@ -0,0 +1,14 @@
+-- FASE D · Bloque 4 · Evangelios 1
+select internal.import_stepbible_tagnt_book(
+  'Mat','MAT',1071,18387,511,
+  '15302b91edc1f52be5716ff692c33044fe664ab49c2a0272d591f175cde36e0b',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Mat-Jhn%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  'ab8eaaeb68e17a1dcfa34e1e9350358f22f03bc2a97244d848750ad81044bc8e'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Mrk','MRK',678,11321,548,
+  'c38c4a6dc85328dd470a3fc1d7d0ef6457ab626ebc488f91209213f0a2886700',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Mat-Jhn%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  'ab8eaaeb68e17a1dcfa34e1e9350358f22f03bc2a97244d848750ad81044bc8e'
+);
---
+++ supabase/migrations/20260802111200_importar_lucas_juan_tagnt.sql
@@ -0,0 +1,14 @@
+-- FASE D · Bloque 4 · Evangelios 2
+select internal.import_stepbible_tagnt_book(
+  'Luk','LUK',1151,19498,643,
+  'cf0bee1a096371634b62e2821b3a80459bd1fcfc7560467e77b0b9a1a17eb77d',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Mat-Jhn%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  'ab8eaaeb68e17a1dcfa34e1e9350358f22f03bc2a97244d848750ad81044bc8e'
+);
+
+select internal.import_stepbible_tagnt_book(
+  'Jhn','JHN',879,15650,426,
+  '496b430ee10429f5f74213a5dea4391d1f3c8144e76db650e96036f6a6c5a0d9',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Mat-Jhn%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  'ab8eaaeb68e17a1dcfa34e1e9350358f22f03bc2a97244d848750ad81044bc8e'
+);
