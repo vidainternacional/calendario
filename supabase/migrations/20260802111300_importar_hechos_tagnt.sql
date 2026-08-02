d3abd985e73b485767ab5ce38d24af7a46fd063e
vidainternacional <publiartsv.info@gmail.com>
2026-08-02 13:13:36 -0600

    data(fase-d): importar Hechos completo

---
+++ supabase/migrations/20260802111300_importar_hechos_tagnt.sql
@@ -0,0 +1,7 @@
+-- FASE D · Bloque 4 · Hechos
+select internal.import_stepbible_tagnt_book(
+  'Act','ACT',1007,18452,534,
+  'de7fecba38d0486025fb7801bcdd119a7431ba61059197f0061aa19072c12449',
+  'https://raw.githubusercontent.com/STEPBible/STEPBible-Data/b86d26cdb1f51729e73b5b4eb7f7ccadc5dfba39/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt',
+  '524e32375361e6d3fa2f7ef00b87605fdc4317a762f395651a05fdc31ad031b7'
+);
