34c4905909ae673d1692f51a1dc059149d268d90
vidainternacional <publiartsv.info@gmail.com>
2026-08-12 22:19:31 -0600

    fix(fase-e): enlazar tema bíblico con estado visual

--- app/(app)/biblia/page.tsx
+++ app/(app)/biblia/page.tsx
@@ -12,6 +12,7 @@ import BibliaProyectoEnhancer from '@/components/biblia/BibliaProyectoEnhancer'
 import BibliaDeepStudyEnhancer from '@/components/biblia/BibliaDeepStudyEnhancer'
 import BibliaQuickReferenceSearch from '@/components/biblia/BibliaQuickReferenceSearch'
 import BibliaReadingFooterEnhancer from '@/components/biblia/BibliaReadingFooterEnhancer'
+import BibliaThemeStateBridge from '@/components/biblia/BibliaThemeStateBridge'
 import { tieneAccesoPastoral } from '@/lib/pastoral/access'
 import './biblia.css'
 import './biblia-first-paint.css'
@@ -65,6 +66,7 @@ export default async function BibliaPage({ searchParams }: { searchParams: Promi
 
       {esProyectoPastoral && <BibliaProyectoEnhancer paqueteId={paqueteId} />}
       <BibliaClient />
+      {from !== 'pastoral' && <BibliaThemeStateBridge />}
       {from !== 'pastoral' && <BibliaQuickReferenceSearch />}
       {from !== 'pastoral' && <BibliaReadingFooterEnhancer />}
       {from !== 'pastoral' && <BibliaDeepStudyEnhancer />}
