c24492f9fccf9e908351ed87c1fca29df25c07bf
vidainternacional <publiartsv.info@gmail.com>
2026-08-14 14:12:10 -0600

    chore(fase-f): renovar caché offline multiorigen

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vida-shell-v1.9-predicacion'
+const CACHE_NAME = 'vida-shell-v2.0-multiorigen'
 const OFFLINE_NOTES_SHELL = '/offline/notas.html'
 const OFFLINE_NOTES_OWNER_MARKER = '/offline/notas-owner'
 const OWNER_UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
