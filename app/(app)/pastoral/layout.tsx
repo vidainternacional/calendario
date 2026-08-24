36b6b2d7f2fd2c0f4f3f290acc52671d9cfa48d5
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 14:35:41 -0600

    style(pastoral): cargar jerarquia visual auditada

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -1,6 +1,7 @@
 import './pastoral-visual-system.css'
 import './pastoral-workspace-v2.css'
 import './pastoral-editor-minimal.css'
+import './pastoral-editor-structure.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
