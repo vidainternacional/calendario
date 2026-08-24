33886319cce8a7151522dcd1800fff2dcf20d6ae
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 16:19:55 -0600

    style(pastoral): activar compactacion movil estable

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -3,6 +3,7 @@ import './pastoral-workspace-v2.css'
 import './pastoral-editor-minimal.css'
 import './pastoral-editor-structure.css'
 import './pastoral-editor-hierarchy-v2.css'
+import './pastoral-editor-compact-mobile.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
