b820128120264c9a1c87aa522af4f373ce01612c
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 15:59:21 -0600

    style(pastoral): activar jerarquia contextual final

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -2,6 +2,7 @@ import './pastoral-visual-system.css'
 import './pastoral-workspace-v2.css'
 import './pastoral-editor-minimal.css'
 import './pastoral-editor-structure.css'
+import './pastoral-editor-hierarchy-v2.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
