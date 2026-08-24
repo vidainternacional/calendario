ffa3060400421978fe77aa80925fa4a5df4078a3
vidainternacional <publiartsv.info@gmail.com>
2026-08-23 20:57:21 -0600

    style(pastoral): activar sistema visual workspace v2

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -1,4 +1,5 @@
 import './pastoral-visual-system.css'
+import './pastoral-workspace-v2.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
