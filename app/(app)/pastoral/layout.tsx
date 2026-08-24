c74581d746b641645015da9b08a6d06bf74a1775
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 13:00:10 -0600

    style(pastoral): cargar refinamiento minimalista del editor

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -1,5 +1,6 @@
 import './pastoral-visual-system.css'
 import './pastoral-workspace-v2.css'
+import './pastoral-editor-minimal.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
