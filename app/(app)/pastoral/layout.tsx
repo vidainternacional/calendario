f12c4c8c3a94b77a9b672a6a152462945f6e9d9e
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 17:59:05 -0600

    style(pastoral): cargar cinta CapCut al final

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -7,6 +7,7 @@ import './pastoral-editor-compact-mobile.css'
 import './pastoral-editor-fixed-workspace.css'
 import './pastoral-editor-v3.css'
 import './pastoral-editor-v3-reset.css'
+import './pastoral-editor-capcut.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
