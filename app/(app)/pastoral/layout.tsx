f74c923aa339c14aa1068e14ed8945b4cb8299e8
vidainternacional <publiartsv.info@gmail.com>
2026-08-24 18:47:22 -0600

    style(pastoral): cargar capa elegante final

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -9,6 +9,7 @@ import './pastoral-editor-v3.css'
 import './pastoral-editor-v3-reset.css'
 import './pastoral-editor-capcut.css'
 import './pastoral-editor-capcut-v2.css'
+import './pastoral-editor-elegance-v4.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
