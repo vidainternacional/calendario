a13cfa96b6878372237c9e95519c855b3cddefd4
vidainternacional <publiartsv.info@gmail.com>
2026-08-25 11:37:08 -0600

    fix(pastoral): hacer Plantillas desplazable y ocupar barra inferior

--- app/(app)/pastoral/layout.tsx
+++ app/(app)/pastoral/layout.tsx
@@ -15,6 +15,7 @@ import './pastoral-editor-authority.css'
 import './pastoral-editor-accessible-v5.css'
 import './pastoral-editor-accessible-v5-authority.css'
 import './pastoral-editor-notes-language-v6.css'
+import './pastoral-editor-plantillas-v7.css'
 
 export default function PastoralLayout({ children }: { children: React.ReactNode }) {
   return <div className="pastoral-visual-system">{children}</div>
